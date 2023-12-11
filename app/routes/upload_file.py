38e952f332a6566b366ddda19f73e464cba1f98d
Muhammad Amin Haiqal <73211654+aminhaiqal@users.noreply.github.com>
2023-12-11 21:15:06 +0800

    Refactor upload_file.py module

--- app/routes/upload_file.py
+++ app/routes/upload_file.py
@@ -1,44 +1,56 @@
-from flask import Blueprint, request, jsonify
-from flask_pymongo import PyMongo
-from werkzeug.utils import secure_filename
-from app.services import doc_ask
+from flask import request, jsonify
+from pymongo import MongoClient
+from bson import ObjectId
+from dotenv import load_dotenv
+from services import doc_processing, img_processing
 import os
 
-upload_route = Blueprint('upload_route', __name__)
+from . import app  # Import the Flask app instance
 
-mongo = PyMongo()
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
-@upload_route.route('/upload', methods=['POST'])
+@app.route('/upload', methods=['POST'])
 def upload_file():
-    if 'file' not in request.files:
+    if 'mediaFile' not in request.files:
         return jsonify({'error': 'No file part'}), 400
 
-    file = request.files['file']
+    file = request.files['mediaFile']
 
     if file.filename == '':
         return jsonify({'error': 'No selected file'}), 400
 
-    if file:
-        # Save the uploaded file to the upload folder
-        filename = secure_filename(file.filename)
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(file_path)
+    if file and allowed_file(file.filename):
+        try:
+            if file.filename.lower().endswith(('.jpg', '.jpeg', '.png', '.gif', '.bmp')):
+                # Process image
+                processed_image = img_processing(file)
+            elif file.filename.lower().endswith('.pdf'):
+                # Process PDF
+                knowledge_base, chunks = doc_processing(file)
+            else:
+                return jsonify({'error': 'Invalid file type'}), 400
 
-        # Process the PDF and extract vector data using the document service
-        knowledge_base, chunks = doc_ask(file_path)
+            # Use the configuration for MongoDB
+            client = MongoClient(app.config['MONGO_URI'])
+            db = client[app.config['DATABASE_NAME']]
+            collection_name = app.config['COLLECTION_NAMES']['pdf_data']
+            collection = db[collection_name]
 
-        # Save the knowledge base and file information to MongoDB
-        document = {
-            'filename': filename,
-            'knowledge_base': knowledge_base.to_dict(),
-            'chunks': chunks
-        }
+            # Save to MongoDB
+            data_to_insert = {
+                'knowledge_base': knowledge_base,
+                'chunks': chunks
+            }
 
-        mongo.db.documents.insert_one(document)
+            result = collection.insert_one(data_to_insert)
 
-        # Remove the uploaded file after processing
-        os.remove(file_path)
+            return jsonify({'message': 'File uploaded successfully', 'document_id': str(result.inserted_id)}), 200
 
-        return jsonify({'message': 'File uploaded successfully'}), 200
+        except Exception as e:
+            return jsonify({'error': str(e)}), 400
 
-    return jsonify({'error': 'Internal server error'}), 500
+    return jsonify({'error': 'Invalid file type'}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
