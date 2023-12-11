08e6782a37bec441483a1a852a539fcd1c136e34
Muhammad Amin Haiqal <73211654+aminhaiqal@users.noreply.github.com>
2023-12-11 21:14:55 +0800

    Add save_document function to document.py

--- app/models/document.py
+++ app/models/document.py
@@ -2,3 +2,15 @@ from app import mongo
 
 def get_document_collection():
     return mongo.db.documents
+
+def save_document(knowledge_base, chunks):
+    document_collection = get_document_collection()
+    
+    document = {
+        'knowledge_base': knowledge_base,
+        'chunks': chunks
+    }
+    
+    document_collection.insert_one(document)
+    
+    return document
