cb5030fadb857ac56423ae950b3acfc180819671
Muhammad Amin Haiqal <73211654+aminhaiqal@users.noreply.github.com>
2023-12-11 21:13:59 +0800

    delete

--- config.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask_pymongo import PyMongo
-
-app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb+srv://<username>:<password>@cluster0.mongodb.net/<dbname>?retryWrites=true&w=majority'
-
-mongo = PyMongo(app)
