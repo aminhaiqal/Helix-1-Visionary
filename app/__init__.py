ae41afa5677df6fd641cda8f91e66c65042c21f7
Muhammad Amin Haiqal <73211654+aminhaiqal@users.noreply.github.com>
2023-12-11 21:14:18 +0800

    Update app initialization and configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,9 @@
 from flask import Flask
-from flask_pymongo import PyMongo
-from app.routes import upload_file
-
-import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'your_mongo_db_uri'
-mongo = PyMongo(app)
+load_dotenv()
 
-# Define the folder where uploaded files will be stored
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config.from_object('app.config.Config')
 
-# Ensure the upload folder exists
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-
-app.register_blueprint(upload_file)
-
-# ... Other app configurations, if any
+from . import routes
